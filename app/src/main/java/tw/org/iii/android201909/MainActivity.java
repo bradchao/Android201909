259ee9b1c6b411475ac01f732e872a01447fedff
brad <brad@brad.tw>
2019-12-06 21:16:41 +0800

    2

--- app/src/main/java/tw/org/iii/android201909/MainActivity.java
+++ app/src/main/java/tw/org/iii/android201909/MainActivity.java
@@ -3,6 +3,13 @@ package tw.org.iii.android201909;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void test1(View view) {
+        try {
+            URL url = new URL("https://www.iii.org.tw");
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.connect();
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            String line;
+            while ( (line = reader.readLine()) != null){
+                Log.v("brad", line);
+            }
+            reader.close();
+
+        }catch (Exception e){
+            Log.v("brad", e.toString());
+        }
+
+
+    }
+    public void test2(View view) {
+
     }
 }
