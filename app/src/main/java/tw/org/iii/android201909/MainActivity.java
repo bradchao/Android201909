ee831042aa9252acc950df39294542e20df494e7
brad <brad@brad.tw>
2019-12-09 19:41:54 +0800

    9

--- app/src/main/java/tw/org/iii/android201909/MainActivity.java
+++ app/src/main/java/tw/org/iii/android201909/MainActivity.java
@@ -20,6 +20,9 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
@@ -118,8 +121,7 @@ public class MainActivity extends AppCompatActivity {
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
-                        Log.v("brad", response);
-                        tv.setText(response);
+                        parseTest4(response);
                     }
                 },
                 new Response.ErrorListener() {
@@ -130,6 +132,27 @@ public class MainActivity extends AppCompatActivity {
                 });
         MainApp.queue.add(request);
     }
+
+    private void parseTest4(String json){
+        try {
+            JSONArray root = new JSONArray(json);
+            Log.v("brad", "count = " + root.length());
+            for (int i=0; i<root.length(); i++){
+                JSONObject row = root.getJSONObject(i);
+                String name = row.getString("Name");
+                String tel = row.getString("Tel");
+                String address = row.getString("Address");
+                Log.v("brad", name+":"+tel+":"+address);
+            }
+
+
+
+
+        }catch (Exception e){
+            Log.v("brad", e.toString());
+        }
+    }
+
 
     private class UIHandler extends Handler {
         @Override
