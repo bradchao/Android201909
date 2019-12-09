0590b6a61467a3593a8292d9841440e5cd73f16a
brad <brad@brad.tw>
2019-12-09 19:51:38 +0800

    10

---
+++ app/src/main/java/tw/org/iii/android201909/MyDBHelper.java
@@ -0,0 +1,23 @@
+package tw.org.iii.android201909;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class MyDBHelper extends SQLiteOpenHelper {
+    public MyDBHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
